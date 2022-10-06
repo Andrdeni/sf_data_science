e71f8b62e24ced62ae33c1da98fafcf8eb99fa48
AndrDeni <andrden86@mail.ru>
2022-10-07 00:46:02 +0300

    update

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -11,16 +11,20 @@ def random_predict(number:int=1) -> int:
     """
 
     count = 0
-
+    max1 = 101
+    min1 = 1
+    number = np.random.randint(1, 101)
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
-        if number == predict_number:
+        mid1 = round((max1 + min1)/2) # предполагаемое число
+        if number == mid1:
             break # выход из цикла, если угадали
-        if count > 20: break 
+        elif number > mid1:
+            min1 = mid1
+        else:
+            max1 = mid1
+        if count > 20: break   
     return(count)
-
-print(f'Количество попыток: {random_predict()}')
 
 def score_game(random_predict) -> int:
     """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
@@ -40,10 +44,8 @@ def score_game(random_predict) -> int:
         count_ls.append(random_predict(number))
 
     score = int(np.mean(count_ls)) # находим среднее количество попыток
-
+    
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
-# RUN
-if __name__ == '__main__':
-    score_game(random_predict)
+print(score_game(random_predict))
