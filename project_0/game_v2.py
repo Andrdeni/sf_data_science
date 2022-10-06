6477cc72eae547483143be9fd7a58f073df6fd94
AndrDeni <andrden86@mail.ru>
2022-10-07 00:07:24 +0300

    update_

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -17,6 +17,7 @@ def random_predict(number:int=1) -> int:
         predict_number = np.random.randint(1, 101) # предполагаемое число
         if number == predict_number:
             break # выход из цикла, если угадали
+        if count > 20: break 
     return(count)
 
 print(f'Количество попыток: {random_predict()}')
